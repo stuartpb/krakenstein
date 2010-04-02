4c9c383afc117b9d814a54f1504e1216cacb72d1
Stuart P. Bentley <Stuart@testtrack4.com>
2010-04-01 22:57:56 -0700

    removed redundant file open in make_krakenbake.lua

--- make_krakenbake.lua
+++ make_krakenbake.lua
@@ -4,12 +4,8 @@ embed=io.open("embedded_krakenbake.wlua","w")
 embed:write"local function embedded(exe_message)\n"
 embed:write(krakenbake,'\n')
 embed:write[=[end
-local external, message=io.open("krakenbake.wlua","r")
-if external then
-  external:close()
-  external, message = loadfile "krakenbake.wlua"
-  if external then return external()
-    else return embedded(message) end
+external, message = loadfile "krakenbake.wlua"
+if external then return external()
 else return embedded(message) end
 ]=]
 embed:close()
